fd5e13facf9aa6c6acb1a14bcf414b18f3ebf3a4
Felix Brüggemann <Felix.brgm@icloud.com>
2023-09-17 15:27:37 +0200

    save

---
+++ source/ft_strlen.s
@@ -0,0 +1,16 @@
+section .text
+    global ft_strlen
+
+ft_strlen:
+    xor rax, rax        ; Reset the return value
+    test rdi, rdi       ; Check for null 
+    jz .found_end       ; And return if null
+    .loop:
+        mov al, [rdi]   ; Move current char into al -> 8 bit register
+        test al, al     ; Sets ZF (zero flag) if zero
+        jz .found_end   ; check if ZF flag is set - if it jumps to jz
+        inc rax
+        inc rdi
+        jmp .loop
+    .found_end:
+        ret
---
+++ source/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+extern int ft_strlen(const char *str);
+int main()
+{
+    printf("Erg: %i\n", ft_strlen("Hello"));
+    return (0);
+}
