84c66089ccaf12aaa30567ded45dfb325a79a64d
FelixBrgm <github.badly321@passinbox.com>
2023-09-19 11:22:48 +0200

    Ft_strcpy done

--- source/main.c
+++ source/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 extern int ft_strlen( char *str);
+extern char *ft_strcpy( char *dst, char *src);
 
 int main()
 {
@@ -9,5 +10,11 @@ int main()
     printf("ft_strlen:\n");
     printf("\t'NULL' -> 0 | %i\n",ft_strlen(NULL));
     printf("\t'some words' -> 10 | %i\n",ft_strlen("some words"));
+
+    char *dst = malloc(4);
+    char *src = "Say";
+    printf("ft_strcpy:\n");
+    printf("\t '%s' | '%s'\n", src,  ft_strcpy(dst, src));
+
     return (0);
 }
