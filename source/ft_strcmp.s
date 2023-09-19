77a6f72f682fc0803668c1b1ed5bdc886c58ff54
Felix Bruggemann <gateway_painter_03@icloud.com>
2023-09-19 18:36:45 +0200

    adsf

--- source/ft_strcmp.s
+++ source/ft_strcmp.s
@@ -4,8 +4,16 @@ section .text
 ft_strcmp:
     ; xor rcx, rcx
     xor rax, rax
-
+.loop:
     mov al, [rdi]
     mov bl, [rsi]
+    test al, al
+    jz .done
+    test bl, bl
+    jz .done
+    
+    cmp rax, rbx
+    je .loop
+.done:
     sub rax, rbx
     ret
