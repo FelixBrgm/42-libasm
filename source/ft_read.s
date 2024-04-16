ef6e98a86df762a0d10774fe90cc8b7000db67c3
Felix Brüggemann <gateway_painter_03@icloud.com>
2024-04-16 10:19:30 +0000

    ADD: erronor read and write

--- source/ft_read.s
+++ source/ft_read.s
@@ -1,7 +1,20 @@
 section .text
     global ft_read
+    extern __errno_location
 
-.ft_read:           ; rax is used to provide the system call number
-    xor rax, rax    ; rax = 0
-    syscall         ; read(rdi, rsi, rdx)
+ft_read:                            ; rax is used to provide the system call number
+    xor rax, rax                    ; rax = 0
+    syscall                         ; read(rdi, rsi, rdx)
+    test rax, rax       
+    js .error
+    ret
+.error
+    NEG rax                         ; rax *= -1
+    push rax                        ; save rax on stack
+
+    call __errno_location wrt ..plt ; rax = __errno_location()
+
+    pop rbx                         ; get rax from stack into rbx
+    mov [rax], rbx                  ; *rax = rbx
+    mov rax, -1                     ; rax = -1
     ret
--- source/main.c
+++ source/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
 extern int  ft_strlen( char *str);
 extern char *ft_strcpy( char *dst, char *src);
 extern int  ft_strcmp(char *s1, char *s2);
 extern char *ft_strdup(char *str);
+extern ssize_t ft_read(int fd, void *buf, size_t count);
+extern ssize_t ft_write(int fd, const void *buf, size_t count);
+
 int main()
 {
     printf("------------------\n");
@@ -31,6 +38,32 @@ int main()
     char *s5 = "TestString";
     printf("\t strdup: |%s|, ft_strdup: |%s|\n", strdup(s4), ft_strdup(s4));
     printf("\t strdup: |%s|, ft_strdup: |%s|\n", strdup(s5), ft_strdup(s5));
+
+
+    char *str = "Hello, world!";
+    char buffer[100];
+    printf("Testing ft_read:\n");
+    int fd = open("source/main.c", O_RDONLY);
+    ssize_t count = read(fd, buffer, 99);
+    buffer[count] = '\0';
+    printf("Expected: \n%s\n", buffer);
+    lseek(fd, 0, SEEK_SET);
+    count = ft_read(fd, buffer, 99);
+    buffer[count] = '\0';
+    printf("Got: \n%s\n", buffer);
+    close(fd);
+
+    printf("Testing ft_write:\n");
+    write(1, str, strlen(str));
+    write(1, "\n", 1);
+    ft_write(1, str, strlen(str));
+    ft_write(1, "\n", 1);
+
+    printf("Tester end...\n");
+    printf("Return value: %li\n", ft_read(0, NULL, 0));
+    printf("Errorno: %i\n", errno);
+    printf("Return value: %li\n", read(0, NULL, 0));
+    printf("Errorno: %i\n", errno);
 
     return (0);
 }
