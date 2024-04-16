3f6708dc86deb71520ba09c49fc61e5bd0d8eabd
FelixBrgm <github.badly321@passinbox.com>
2024-04-16 10:29:14 +0000

    FIX: strcmp based on tester

--- source/ft_strcmp.s
+++ source/ft_strcmp.s
@@ -9,13 +9,18 @@ ft_strcmp:
     mov al, [rdi + rcx]     ; al = rdi[rcx]
     mov bl, [rsi + rcx]		; bl = rsi[rcx]
     inc rcx					; rcx++
+
     test al, al				; al == 0
-    jz .done
+    jz .on_null_end
     test bl, bl				; bl == 0
-    jz .done
+    jz .on_null_end
     
     sub rax, rbx			; rax -= rbx
     test rax, rax			; rax == 0
     jz .loop
+		
 .done:
+    ret
+.on_null_end:
+    sub rax, rbx	        ; rax -= rbx
     ret
