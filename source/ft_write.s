c1a60dce784e02c29d08634135efa2118501690b
FelixBrgm <github.badly321@passinbox.com>
2024-04-16 10:19:30 +0000

    ADD: erronor read and write

--- source/ft_write.s
+++ source/ft_write.s
@@ -1,7 +1,20 @@
 section .text
     global ft_write
+    extern __errno_location
 
 ft_write:           ; rax is used to provide the system call number
-   mov rax, 1       ; rax = 1
-   syscall          ; write(rdi, rsi, rdx)
-   ret
+    mov rax, 1       ; rax = 1
+    syscall          ; write(rdi, rsi, rdx)
+    test rax, rax       
+    js .error
+    ret
+.error:
+    NEG rax                         ; rax *= -1
+    push rax                        ; save rax on stack
+
+    call __errno_location wrt ..plt ; rax = __errno_location()
+
+    pop rbx                         ; get rax from stack into rbx
+    mov [rax], rbx                  ; *rax = rbx
+    mov rax, -1                     ; rax = -1
+    ret
